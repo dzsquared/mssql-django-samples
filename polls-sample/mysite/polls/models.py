f9c9370766adbe1dd20cc020865652a5c1e42768
Drew Skwiers-Koballa <dzsquared@users.noreply.github.com>
2021-09-28 14:40:01 +0000

    remove errant column

--- polls-sample/mysite/polls/models.py
+++ polls-sample/mysite/polls/models.py
@@ -7,7 +7,6 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    created_by = models.CharField(max_length=100)
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
